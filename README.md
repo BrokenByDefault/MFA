4c15c93b835f22bf9d36d615e387037c63db8f6c
Inkeliz <inkeliz@inkeliz.com>
2018-09-13 23:52:23 -0300

    Release of V3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# MFA
+# TwoFactor Android
+
+-----------------
+
+Source code used for TwoFactor are hosted in [https://github.com/BrokenByDefault/Nanollet/tree/master/TwoFactor](https://github.com/BrokenByDefault/Nanollet/tree/master/TwoFactor).
--- www/js/main.go
+++ www/js/main.go
@@ -4,9 +4,7 @@
 package main
 
 import (
-	"github.com/gopherjs/gopherjs/js"
 	"honnef.co/go/js/dom"
-	"github.com/brokenbydefault/MFA"
 	"github.com/brokenbydefault/Nanollet/GUI/guitypes"
 	"github.com/brokenbydefault/Nanollet/GUI/Front"
 	"github.com/brokenbydefault/Nanollet/GUI/App/DOM"
@@ -14,18 +12,34 @@ import (
 	"github.com/jaracil/goco/nativestorage"
 	"github.com/jaracil/goco/barcodescanner"
 	"strings"
-	"github.com/brokenbydefault/Nanollet/RPC"
-	"github.com/brokenbydefault/Nanollet/RPC/Connectivity"
 	"github.com/brokenbydefault/Nanollet/Util"
 	"github.com/jaracil/goco"
 	"github.com/jaracil/goco/dialogs"
+	"github.com/brokenbydefault/Nanollet/TwoFactor"
+	"encoding/binary"
+	"bytes"
 )
 
-func OnDeviceReady(cb func()) {
-	js.Global.Get("document").Call("addEventListener", "deviceready", cb, false)
-}
-
 var root = dom.GetWindow().Document()
+
+func main() {
+	goco.OnDeviceReady(func() {
+		go func() {
+			if _, err := nativestorage.GetString("DEVICE"); err != nil {
+				_, sk, _ := Wallet.GenerateRandomKeyPair()
+				nativestorage.SetItem("DEVICE", Util.SecureHexEncode(sk[:]))
+			}
+
+			InitApplication(root, &AccountApp{})
+			ViewApplication(root, &AccountApp{})
+
+			if _, err := nativestorage.GetString("SEEDFY"); err == nil {
+				ViewPage(root, &PagePassword{})
+			}
+
+		}()
+	})
+}
 
 type AccountApp guitypes.App
 
@@ -78,13 +92,13 @@ func (c *PageGenerate) Name() string {
 func (c *PageGenerate) OnView(w dom.Document) {
 	page := DOM.SetSector(c)
 
-	seed, err := MFA.NewSeedFY()
+	seed, err := TwoFactor.NewSeedFY()
 	if err != nil {
 		return
 	}
 
 	textarea, _ := page.SelectFirstElement(w, ".seed")
-	textarea.SetTextContent(seed.Encode())
+	textarea.SetTextContent(seed.String())
 	DOM.ReadOnlyElement(textarea)
 }
 
@@ -99,7 +113,7 @@ func (c *PageGenerate) OnContinue(w dom.Document, _ string) {
 
 	err = nativestorage.SetItem("SEEDFY", seed)
 	if err != nil {
-		dialogs.Alert("Imposible to store the seed", "Error", "Ok")
+		dialogs.Alert("Impossible to store the seed", "Error", "Ok")
 		return
 	}
 
@@ -136,7 +150,7 @@ func (c *PageImport) OnContinue(w dom.Document, _ string) {
 	}
 
 	if err = nativestorage.SetItem("SEEDFY", seed); err != nil {
-		dialogs.Alert("Imposible to store the seed", "Error", "Ok")
+		dialogs.Alert("Impossible to store the seed", "Error", "Ok")
 		return
 	}
 
@@ -165,26 +179,18 @@ func (c *PagePassword) OnContinue(w dom.Document, _ string) {
 	DOM.ApplyForIt(w, ".password", DOM.ClearValue)
 
 	seed, _ := nativestorage.GetString("SEEDFY")
-	seedfy, err := MFA.ReadSeedFY(seed, password)
-	if err != nil {
-		return
-	}
-
-	token, err := MFA.RecoverToken(seedfy, 0)
+	token, err := TwoFactor.NewToken(seed, []byte(password))
 	if err != nil {
 		return
 	}
 
 	devicehex, _ := nativestorage.GetString("DEVICE")
-	device, ok := Util.SecureHexDecode(devicehex)
+	deviceb, ok := Util.SecureHexDecode(devicehex)
 	if !ok {
 		return
 	}
 
-	sender, err := MFA.NewSender(MFA.NewDevice(device))
-	if err != nil {
-		return
-	}
+	device := Wallet.NewSecretKey(deviceb)
 
 	qrcode, err := barcodescanner.NewScanner().Scan()
 	if err != nil {
@@ -192,41 +198,17 @@ func (c *PagePassword) OnContinue(w dom.Document, _ string) {
 		return
 	}
 
+	request := TwoFactor.Request{}
 	receiver, ok := Util.SecureHexDecode(qrcode.Text)
-	if len(receiver) != 32 || !ok {
+	if err := binary.Read(bytes.NewReader(receiver), binary.BigEndian, &request); err != nil || !ok {
 		dialogs.Alert("Impossible to decode the key", "Error", "Ok")
 		return
 	}
 
-	env, err := sender.CreateEnvelope(receiver, token)
-	if err != nil {
-		return
-	}
-
-	err = RPCClient.SendToken(Connectivity.Socket, receiver, env)
-	if err != nil {
+	if err := TwoFactor.ReplyRequest(&device, token, request); err != nil {
 		dialogs.Alert("Impossible to connect with our server", "Error", "Ok")
 		return
 	}
 
 	return
-}
-
-func main() {
-	goco.OnDeviceReady(func() {
-		go func() {
-			if _, err := nativestorage.GetString("DEVICE"); err != nil {
-				nativestorage.SetItem("DEVICE", Util.SecureHexEncode(MFA.GenerateDevice().SecretKey))
-			}
-
-			InitApplication(root, &AccountApp{})
-			ViewApplication(root, &AccountApp{})
-
-			if _, err := nativestorage.GetString("SEEDFY"); err == nil {
-				ViewPage(root, &PagePassword{})
-			}
-
-			Connectivity.Socket.StartWebsocket()
-		}()
-	})
 }
